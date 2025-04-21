c11432fc447f3c8fb3c029c5d1b6e5a090673367
Sumdiboii <spimplikar08@gmail.com>
2025-04-21 12:02:35 +0530

    home page is almost complete and responsive...project should take 2 more weeks tbh

---
+++ src/components/home-components/credits-button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './credits-button.css'; // Optional CSS file for styling
+
+const Credits = () => {
+  return (
+    <button className="credit-button">
+      <span className="bar bar1"></span>
+      <span className="bar bar2"></span>
+      <span className="bar bar1"></span>
+    </button>
+  );
+};
+
+export default Credits;
--- src/final-pages/home.jsx
+++ src/final-pages/home.jsx
@@ -1,15 +1,46 @@
-// home.jsx
 import React from 'react';
 import Stars from '../components/loader-components/stars';
 import SettingsButton from '../components/home-components/settings-button';
+import './Home.css'; // Make sure to import your CSS
+import Credits from '../components/home-components/credits-button';
 
 const Home = () => {
   return (
+    <div className="home">
+    <Stars />
     <div className="home-page">
-        <Stars />
-        <SettingsButton />
-      <h1>Welcome to Minesweeper!</h1>
-      <p>This is the demo page after the loader.</p>
+      
+
+      <div className="split-container">
+        <div className="left-panel">
+          <div className="left-top">
+          <img
+        src="/assets/welcome.png"
+        alt="Switch to Landscape Orientation"
+        className="rotate-warning-image"
+      />
+   
+  </div>
+  <div className="left-bottom">
+  <div className="container-left">
+    <SettingsButton />
+  </div>
+  <div className="container-right">
+    <Credits />
+  </div>
+</div>
+
+        </div>
+        <div className="right-panel">
+  <div className="container-1">Free Play</div>
+  <div className="container-2">Time Battle</div>
+  <div className="container-3">Multiplayer</div>
+</div>
+
+      </div>
+
+      
+    </div>
     </div>
   );
 };
