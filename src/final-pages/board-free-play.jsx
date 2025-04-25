e6004cb21d712f5ce821c48c1b8c1c952a3dc34f
Sumdiboii <spimplikar08@gmail.com>
2025-04-25 20:37:57 +0530

    the game logic and board is pending now...made some adjustments...project should take a month..

--- src/final-pages/board-free-play.jsx
+++ src/final-pages/board-free-play.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import './board-free-play.css';
 import Stars from '../components/loader-components/stars';
 
+
 const FreePlay = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(false); // default to false
 
   return (
     <div className="bg-fpb">
-     {/* <Stars />  */}
+      <Stars />  
 
       {/* Navbar */}
       <div className="navbar-gb">
@@ -18,14 +19,22 @@ const FreePlay = () => {
       {/* Sidebar */}
       <div className={`side-panel ${isPanelOpen ? 'open' : ''}`}>
         <button className="close-btn" onClick={() => setIsPanelOpen(false)}>✖</button>
-        <div className="quick-access-control-box"></div>
-        <div className="music-player"></div>
-        <div className="game-controls"></div>
-        <div className="board-switch-quit"></div>
+        <div className="quick-access-control-box">quick-access-control-box ---dnd, silent, record, screenshot..</div>
+        
+        <div className="game-controls">game controls</div>
+        <div className="focus-mode">focus mode full screen no animations just play tyrhard :0</div>
       </div>
 
       {/* Game Section */}
       <div className="freeplay-container">
+        <div className="game-info-box">
+            <div className="timer">timer</div>
+            <div className="shortcutkeys">shortcut keys</div>
+            <div className="music-player">music player</div>
+            <div className="board-switch-fpb-game">board switch</div>
+            <div className="new-quit-button">new / quit button</div>
+
+        </div>
         <div className="game-board">
          
           {/* Add game components here */}
